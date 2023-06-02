415118af6a46fd8d31af1c797bfe484a8150dc6b
Ankit <114883565+Ankitsatyam@users.noreply.github.com>
2023-06-02 14:33:30 +0530

    initial setup

---
+++ uni-frontend/src/pages/global/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './login.module.css'
+
+const Login = () => {
+  return (
+    <div className={classes.ctn}>
+      
+    </div>
+  )
+}
+
+export default Login
